5445a70282be3b2d6349c3b8231bc8aa944001fd
Philippe Canal <pcanal@fnal.gov>
2007-02-05 18:57:58 +0000

    Add support for ResourceType
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk/probe@455 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- pbs-lsf/pbs-lsf.py
+++ pbs-lsf/pbs-lsf.py
@@ -5,7 +5,7 @@ import Gratia, sys
 if __name__ == '__main__':
         Gratia.Initialize()
 	if hasattr(sys,'argv') and sys.argv[1]:
-		print Gratia.SendXMLFiles(sys.argv[1], True)
-#		print Gratia.SendXMLFiles(sys.argv[1], False)
+		print Gratia.SendXMLFiles(sys.argv[1], True, "Batch")
+#		print Gratia.SendXMLFiles(sys.argv[1], False, "Batch")
 	else:
 		print "No records directory specified"
