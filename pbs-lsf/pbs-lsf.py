1e16f61586fab5ba6249be596eb240b918367f71
Chris Green <greenc@fnal.gov>
2008-10-17 23:26:56 +0000

    Set the batch manager name as sent by pbs-lsf_meter.pl.
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk/probe@2726 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- pbs-lsf/pbs-lsf.py
+++ pbs-lsf/pbs-lsf.py
@@ -3,14 +3,15 @@
 import Gratia, sys
 
 if __name__ == '__main__':
-        rev = Gratia.ExtractCvsRevision("$Revision: 1.6 $");
+        rev = Gratia.ExtractCvsRevision("$Revision: 1.7 $");
         tag = Gratia.ExtractCvsRevision("$Name: not supported by cvs2svn $");
         Gratia.RegisterReporter("pbs-lsf.py", str(rev) + " (tag " + str(tag) + ")")
 	if hasattr(sys,'argv') and sys.argv[1]:
+                if (len(sys.argv) >= 3 and sys.argv[2]):
+                        Gratia.setProbeBatchManager(sys.argv[2])
                 if (len(sys.argv) == 4) and sys.argv[2] and sys.argv[3]:
                         Gratia.RegisterService(sys.argv[2], sys.argv[3])
                 Gratia.Initialize()
 		print Gratia.SendXMLFiles(sys.argv[1], True, "Batch")
-#		print Gratia.SendXMLFiles(sys.argv[1], False, "Batch")
 	else:
-		Gratia.DebugPrint(-1, "No records directory specified")
+		print "pbs-lsf.py: No records directory specified\n"
