b673a168cf9050daae43152f18ee9d11ebbd4cb4
Philippe Canal <pcanal@fnal.gov>
2010-02-01 17:35:50 +0000

    avoid skipping the execution of condor_meter.pl unless we really started it
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk/probe@3853 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- condor/condor_meter.cron.sh
+++ condor/condor_meter.cron.sh
@@ -38,95 +38,111 @@ do
   fi
 done
 
+# Set the working directory, where we expect to find the following
+#    necessary files.
+if [ -d ${Meter_BinDir} ]; then
+  cd ${Meter_BinDir}
+else
+  ${Logger} "No such directory ${Meter_BinDir}"
+  exit -1
+fi
+
 # Need to be sure there is not one of these running already
 #   This may not be the best way to test this for the long term ??? ###
 NCMeter=`ps -ef | grep condor_meter.pl | grep -v grep | wc -l`
-if [ ${NCMeter} -eq 0 ]; then
+eval `grep WorkingFolder ./ProbeConfig`
+if [ ${NCMeter} -ne 0 -a -e ${WorkingFolder}/condor_meter.cron.pid ]; then
+  # We might have a condor_meter.pl running, let's verify that we 
+  # started it.
   
-  # Set the working directory, where we expect to find the following
-  #    necessary files.
-  if [ -d ${Meter_BinDir} ]; then
-    cd ${Meter_BinDir}
-  else
-    ${Logger} "No such directory ${Meter_BinDir}"
-    exit -1
+  otherpid=`cat ${WorkingFolder}/condor_meter.cron.pid`
+  NCCron=`ps -ef | grep ${otherpid} | grep condor_meter.cron | wc -l`
+  if [ ${NCCron} -ne 0 ]; then 
+ 
+     ${Logger} "There is a 'condor_meter.pl' task running already."
+     exit 0
   fi
-  
-  # We need to locate the condor probe script and it must be executable
-  if [ ! -x condor_meter.pl ]; then
-    ${Logger} "The condor_meter.pl file is not in this directory: $(pwd)"
-    exit -2
-  fi
-  
-  # We need to locate these files and they must be readable
-  for Needed_File in ProbeConfig
-  do
-    if [ ! -f ${Needed_File} ]; then
-      ${Logger} \
-       "The ${Needed_File} file is not in this directory: $(pwd)"
-      exit -3
-    fi
-  done
-  
-  pp_dir=$(cd "$Meter_BinDir/../common"; pwd)
-  if test -n "$PYTHONPATH" ; then
-    if echo "$PYTHONPATH" | grep -e ':$' >/dev/null 2>&1; then
-      PYTHONPATH="${PYTHONPATH}${pp_dir}:"
-    else
-      PYTHONPATH="${PYTHONPATH}:${pp_dir}"
-    fi
-  else
-    PYTHONPATH="${pp_dir}"
-  fi
-  export PYTHONPATH
-
-  enabled=`${pp_dir}/GetProbeConfigAttribute.py EnableProbe`
-  (( status = $? ))
-  if (( $status != 0 )); then
-    echo "ERROR checking probe configuration!" 1>&2
-    exit $status
-  fi
-  if [[ -n "$enabled" ]] && [[ "$enabled" == "0" ]]; then
-    ${pp_dir}/DebugPrint.py -l -1 "Probe is not enabled: check $Meter_BinDir/ProbeConfig."
-    exit 1
-  fi
-
-  # This is what we expect in a normal Gratia install
-  DataFolder=`${pp_dir}/GetProbeConfigAttribute.py DataFolder`
-  if [ ! -d ${DataFolder} ]; then
-    ${Logger} "There is no ${DataFolder} directory (defined as DataFolder in ProbeConfig)."
-    exit -4
-  fi
-
-  WorkingFolder=`${pp_dir}/GetProbeConfigAttribute.py WorkingFolder`
-  if [ ! -d ${WorkingFolder} ]; then
-    ${Logger} "There is no ${WorkingFolder} directory (defined as WorkingFolder in ProbeConfig)."
-    exit -4
-  fi
-
-  #echo "Begin processing directory ${DataFolder}"
-  # The '-d' option tells the meter to delete log files after they are
-  #    reported to Gratia.
-  # The '-s' option gives the location of the state file for globus-condor.log
-  ./condor_meter.pl \
-    -d \
-    -v \
-    -s "${WorkingFolder}/globus-condor-log-state.dat" \
-    ${DataFolder} | ${pp_dir}/DebugPrint.py -l 1
-  ExitCode=$?
-  # If the probe ended in error, report this in Syslog and exit
-  if [ $ExitCode != 0 ]; then
-    ${pp_dir}/DebugPrint.py -l -1 "ALERT: $0 exited abnormally with [$ExitCode]"
-    exit $ExitCode
-  fi
-    
-  # Possibly loop to see if there are any new files before exiting. ??? ###
-    
-  # The following debug statement needs to be removed after testing. ###
-  # ls -ltrAF /tmp/py*
-else
-  ${Logger} "There is a 'condor_meter.pl' task running already."
 fi
+
+# We need to locate the condor probe script and it must be executable
+if [ ! -x condor_meter.pl ]; then
+  ${Logger} "The condor_meter.pl file is not in this directory: $(pwd)"
+  exit -2
+fi
+  
+# We need to locate these files and they must be readable
+for Needed_File in ProbeConfig
+do
+  if [ ! -f ${Needed_File} ]; then
+    ${Logger} \
+     "The ${Needed_File} file is not in this directory: $(pwd)"
+    exit -3
+  fi
+done
+  
+pp_dir=$(cd "$Meter_BinDir/../common"; pwd)
+if test -n "$PYTHONPATH" ; then
+  if echo "$PYTHONPATH" | grep -e ':$' >/dev/null 2>&1; then
+    PYTHONPATH="${PYTHONPATH}${pp_dir}:"
+  else
+    PYTHONPATH="${PYTHONPATH}:${pp_dir}"
+  fi
+else
+  PYTHONPATH="${pp_dir}"
+fi
+export PYTHONPATH
+
+enabled=`${pp_dir}/GetProbeConfigAttribute.py EnableProbe`
+(( status = $? ))
+if (( $status != 0 )); then
+  echo "ERROR checking probe configuration!" 1>&2
+  exit $status
+fi
+if [[ -n "$enabled" ]] && [[ "$enabled" == "0" ]]; then
+  ${pp_dir}/DebugPrint.py -l -1 "Probe is not enabled: check $Meter_BinDir/ProbeConfig."
+  exit 1
+fi
+
+# This is what we expect in a normal Gratia install
+DataFolder=`${pp_dir}/GetProbeConfigAttribute.py DataFolder`
+if [ ! -d ${DataFolder} ]; then
+  ${Logger} "There is no ${DataFolder} directory (defined as DataFolder in ProbeConfig)."
+  exit -4
+fi
+
+WorkingFolder=`${pp_dir}/GetProbeConfigAttribute.py WorkingFolder`
+if [ ! -d ${WorkingFolder} ]; then
+  ${Logger} "There is no ${WorkingFolder} directory (defined as WorkingFolder in ProbeConfig)."
+  exit -4
+fi
+
+echo $$ > ${WorkingFolder}/condor_meter.cron.pid
+(( status = $? ))
+if (( $status != 0 )); then
+   ${Logger} "condor_meter.cron.sh failed to store the pid in  ${WorkingFolder}/condor_meter.cron.pid"
+   exit -2
+fi 
+
+#echo "Begin processing directory ${DataFolder}"
+# The '-d' option tells the meter to delete log files after they are
+#    reported to Gratia.
+# The '-s' option gives the location of the state file for globus-condor.log
+./condor_meter.pl \
+  -d \
+  -v \
+  -s "${WorkingFolder}/globus-condor-log-state.dat" \
+  ${DataFolder} | ${pp_dir}/DebugPrint.py -l 1
+ExitCode=$?
+# If the probe ended in error, report this in Syslog and exit
+if [ $ExitCode != 0 ]; then
+  ${pp_dir}/DebugPrint.py -l -1 "ALERT: $0 exited abnormally with [$ExitCode]"
+  exit $ExitCode
+fi
+    
+# Possibly loop to see if there are any new files before exiting. ??? ###
+    
+# The following debug statement needs to be removed after testing. ###
+# ls -ltrAF /tmp/py*
 
 exit 0
 
