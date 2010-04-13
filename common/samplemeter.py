0e78c2dc3a376e32bc10b90818ebbabc8b082d2f
Philippe Canal <pcanal@fnal.gov>
2010-04-13 15:30:08 +0000

    pylint-ing
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk/probe@3983 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- common/samplemeter.py
+++ common/samplemeter.py
@@ -1,62 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+An example on how to create and upload Gratia record.
+"""
 import Gratia
 
+def GetRecord(jobid=0):
+    """ 
+    Create a sample Gratia record
+    """
+    record = Gratia.UsageRecord('Batch')
 
-class Simple:
-        "A simple example class"
-        i = 12345
-        def f(self):
-                return 'hello world'
+    record.LocalUserId('cmsuser000')
+    record.GlobalUsername('john ainsworth')
+    record.DN('CN=john ainsworth, L=MC, OU=Manchester, O=eScience, C=UK')
 
-def GetRecord(jobid = 0):
-        r = Gratia.UsageRecord("Batch")
+    record.LocalJobId('PBS.1234.0bad')
+    record.LocalJobId('PBS.1234.' + str(jobid))  # overwrite the previous entry
 
-        r.LocalUserId("cmsuser000")
-        r.GlobalUsername("john ainsworth")
-        r.DN("CN=john ainsworth, L=MC, OU=Manchester, O=eScience, C=UK")
+    record.JobName('cmsreco ', 'this is not a real job name')
+    record.Charge('1240')
+    record.Status('4')
+    record.Status(4)
 
-        r.LocalJobId("PBS.1234.0bad")
-        r.LocalJobId("PBS.1234." + str(jobid))        # overwrite the previous entry
+    record.Njobs(3, 'Aggregation over 10 days')
 
-        r.JobName("cmsreco ","this is not a real job name")
-        r.Charge("1240")
-        r.Status("4")
-        r.Status(4)
+    record.Network(3.5, 'Gb', 30, 'total')
 
-        r.Njobs(3,"Aggregation over 10 days")
+    # record.Disk(3.5, "Gb", 13891, "max")
+    # record.Memory(650000, "KB", "min")
+    # record.Swap(1.5, "GB", "max")
 
-        r.Network(3.5,"Gb",30,"total")
-        #r.Disk(3.5,"Gb",13891,"max")
-        #r.Memory(650000,"KB","min")
-        #r.Swap(1.5,"GB","max")
-        r.ServiceLevel("BottomFeeder","QOS")
+    record.ServiceLevel('BottomFeeder', 'QOS')
 
-        r.TimeDuration(24,"submit")
-        r.TimeInstant("2005-11-02T15:48:39Z","submit")
+    record.TimeDuration(24, 'submit')
+    record.TimeInstant('2005-11-02T15:48:39Z', 'submit')
 
-        r.WallDuration(6000*3600*25+63*60+21.2,"Was entered in seconds")
-        r.CpuDuration("PT23H12M1.75S","user","Was entered as text")
-        r.CpuDuration("PT12M1.75S","sys","Was entered as text")
-        r.NodeCount(3) # default to total
-        r.Processors(3,.75,"total")
-        r.StartTime(1130946550,"Was entered in seconds")
-        r.EndTime("2005-11-03T17:52:55Z","Was entered as text")
-        r.MachineName("flxi02.fnal.gov")
-        r.SubmitHost("patlx7.fnal.gov")
-        r.Host("flxi02.fnal.gov",True)
-        r.Queue("CepaQueue")
+    record.WallDuration(6000 * 3600 * 25 + 63 * 60 + 21.2, 
+                   'Was entered in seconds')
+    record.CpuDuration('PT23H12M1.75S', 'user', 'Was entered as text')
+    record.CpuDuration('PT12M1.75S', 'sys', 'Was entered as text')
+    record.NodeCount(3)  # default to total
+    record.Processors(3, .75, 'total')
+    record.StartTime(1130946550, 'Was entered in seconds')
+    record.EndTime('2005-11-03T17:52:55Z', 'Was entered as text')
+    record.MachineName('flxi02.fnal.gov')
+    record.SubmitHost('patlx7.fnal.gov')
+    record.Host('flxi02.fnal.gov', True)
+    record.Queue('CepaQueue')
 
-        r.ProjectName("cms reco")
+    record.ProjectName('cms reco')
 
-        r.AdditionalInfo("RemoteWallTime",94365)
-        r.Resource("RemoteCpuTime","PT23H")
+    record.AdditionalInfo('RemoteWallTime', 94365)
+    record.Resource('RemoteCpuTime', 'PT23H')
 
-        return r
+    return record
 
-if __name__ == '__main__': 
-        rev = "$Revision$"
-        Gratia.RegisterReporterLibrary("samplemeter.py",Gratia.ExtractSvnRevision(rev))
-        
-        Gratia.Initialize()
 
-        r = GetRecord()
-        print Gratia.Send(r)
+if __name__ == '__main__':
+    rev = '$Revision$'
+    Gratia.RegisterReporterLibrary('samplemeterecord.py', 
+                                   Gratia.ExtractSvnRevision(rev))
+
+    Gratia.Initialize()
+
+    rec = GetRecord()
+    print Gratia.Send(rec)
