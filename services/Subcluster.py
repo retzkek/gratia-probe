943a064b62aa9d72abe2d7fead5f9a144efabfed
Philippe Canal <pcanal@fnal.gov>
2010-04-14 15:31:37 +0000

    pylint
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/trunk/probe@4000 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- services/Subcluster.py
+++ services/Subcluster.py
@@ -12,16 +12,18 @@ class Subcluster(Gratia.Record):
 
     def __init__(self):
         # Initializer
-        super(self.__class__,self).__init__()
-        DebugPrint(0,"Creating a Subcluster Record"+TimeToString())
+        super(self.__class__, self).__init__()
+        DebugPrint(0, "Creating a Subcluster Record"+TimeToString())
 
     def Print(self):
-        DebugPrint(1,"Subcluster: ",self)
+        DebugPrint(1, "Subcluster: ", self)
         
     def XmlAddMembers(self):
-        " This should add the value of the 'data' member of Subcluster "
-        " (as opposed to the information entered directly into self.RecordData "
-        super(self.__class__,self).XmlAddMembers()
+        """
+        This should add the value of the 'data' member of Subcluster
+        (as opposed to the information entered directly into self.RecordData
+        """
+        super(self.__class__, self).XmlAddMembers()
 
     def XmlCreate(self):
         global RecordId
@@ -43,84 +45,84 @@ class Subcluster(Gratia.Record):
             self.XmlData.append("\n")
         self.XmlData.append("</Subcluster>\n")
 
-    def UniqueID(self,value):
+    def UniqueID(self, value):
         "GlueSubClusterUniqueID"
         self.RecordData = self.AddToList(self.RecordData, "UniqueID", "", value)
         
-    def Name(self,value):
+    def Name(self, value):
         "GlueSubClusterName"
         self.RecordData = self.AddToList(self.RecordData, "Name", "", value)
         
-    def Cluster(self,value):
+    def Cluster(self, value):
         """
         GlueClusterName
         """
         self.RecordData = self.AddToList(self.RecordData, "Cluster", "", value)
        
-    def Platform(self,value):
+    def Platform(self, value):
         """
         Processor Platform
         """
         self.RecordData = self.AddToList(self.RecordData, "Platform", "", value)
  
-    def OS(self,value):
+    def OS(self, value):
         """
         GlueHostOperatingSystemName
         """
         self.RecordData = self.AddToList(self.RecordData, "OS", "", value)
 
-    def OSVersion(self,value):
+    def OSVersion(self, value):
         """
         GlueHostOperatingSystemRelease
         """
         self.RecordData = self.AddToList(self.RecordData, "OSVersion", "", value)
 
-    def Timestamp(self,value):
-        " The time the record was gathered "
-        " Expressed in number of second since epoch or a string formated using the format xsd:dateTime. "
+    def Timestamp(self, value):
+        """ The time the record was gathered
+        Expressed in number of second since epoch or a string formated using the format xsd:dateTime. """
         if isinstance(value, types.StringType):
             realvalue = value
         else:
             realvalue = TimeToString(time.gmtime(value))
         self.AppendToList(self.RecordData, "Timestamp", "", realvalue)
         
-    def Cores(self,value):
+    def Cores(self, value):
         """
         Cores
         """
         self.RecordData = self.AddToList(self.RecordData, "Cores", "", str(value))
  
-    def Hosts(self,value):
+    def Hosts(self, value):
         """
         Hosts
         """
         self.RecordData = self.AddToList(self.RecordData, "Hosts", "", str(value))
  
-    def Cpus(self,value):
+    def Cpus(self, value):
         """
         GlueCEPolicyMaxRunningJobs
         """
         self.RecordData = self.AddToList(self.RecordData, "Cpus", "", str(value))
  
-    def RAM(self,value):
+    def RAM(self, value):
         """
         RAM
         """
         self.RecordData = self.AddToList(self.RecordData, "RAM", "", str(value))
  
-    def Processor(self,value):
+    def Processor(self, value):
         """
         Processor name (from /etc/cpuinfo)
         """
         self.RecordData = self.AddToList(self.RecordData, "Processor", "", str(value))
  
-    def BenchmarkName(self,value):
+    def BenchmarkName(self, value):
         """
         Name of benchmark measurement recorded
         """
         self.RecordData = self.AddToList(self.RecordData, "BenchmarkName", "", value)
  
-    def BenchmarkValue(self,value):
+    def BenchmarkValue(self, value):
         """
         Value of benchmark measurement recorded
         """
@@ -131,26 +133,28 @@ def getSubclusters(xmlDoc):
     namespace = xmlDoc.documentElement.namespaceURI
     return xmlDoc.getElementsByTagNameNS(namespace, 'Subcluster')
 
-def SubclusterCheckXmldoc(xmlDoc,external,resourceType = None):
-    " Fill in missing field in the xml document if needed "
-    " If external is true, also check for ProbeName, SiteName "
+def SubclusterCheckXmldoc(xmlDoc, external, resourceType = None):
+    """
+    Fill in missing field in the xml document if needed
+    If external is true, also check for ProbeName, SiteName
+    """
 
     if not xmlDoc.documentElement:
         return 0 # Major problem
 
     # Local namespace
-    namespace = xmlDoc.documentElement.namespaceURI
+    # namespace = xmlDoc.documentElement.namespaceURI
     # Loop over (posibly multiple) jobUsageRecords
-    for Subcluster in getSubclusters(xmlDoc):
+    for subClusterNode in getSubclusters(xmlDoc):
         # Local namespace and prefix, if any
         prefix = ""
-        for child in Subcluster.childNodes:
+        for child in subClusterNode.childNodes:
             if child.nodeType == xml.dom.minidom.Node.ELEMENT_NODE and \
                 child.prefix:
                 prefix = child.prefix + ":"
                 break
                                
-        StandardCheckXmldoc(xmlDoc,Subcluster,external,prefix)
+        StandardCheckXmldoc(xmlDoc, subClusterNode, external, prefix)
             
     return len(getSubclusters(xmlDoc))
 
