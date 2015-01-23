2badacadbe38e7c43864ae5371fc80bc4310e9bf
Marco Mambelli <marcom@fnal.gov>
2015-01-23 18:29:52 +0000

    fixed named cursor use, added fetchmany for older psycopg, code cleanup
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/branches/dev/sample-probe/probe@5533 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- common2/gratia/common2/pgpinput.py
+++ common2/gratia/common2/pgpinput.py
@@ -7,6 +7,7 @@ import traceback
 # For PostgresSQL
 import psycopg2
 import psycopg2.extras
+import uuid  # for unique cursor name
 
 from gratia.common.Gratia import DebugPrint
 
@@ -37,13 +38,16 @@ class PgInput(DbInput):
     list	ARRAY
     tuple, namedtuple   Composite types
     dict	hstore
-    Psycopg’s Range	range
-    Anything™	json
+    Psycopg's Range	range
+    Anything(TM)	json
     uuid	uuid
     """
 
     def __init__(self, conn=None):
         DbInput.__init__(self)
+        # PsycoPG 2.4 or greater support itersize, so that iterable named cursor
+        # is not fetching only 1 row at the time
+        self.support_itersize = True
         self._cursor = None
         if conn:
             self._connection = conn
@@ -70,7 +74,7 @@ class PgInput(DbInput):
         DebugPrint(4, "Connecting to PgSQL database: %s" % dburl)
         try:
             self._connection = psycopg2.connect(dburl)
-            self._cursor = self._connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            self._cursor = self._get_cursor(self._connection)
         except:
             tblist = traceback.format_exception(sys.exc_type,
                                                 sys.exc_value,
@@ -81,6 +85,28 @@ class PgInput(DbInput):
             # Masking connection failure
             #self._connection = None
         return self._connection
+
+    def _get_cursor(self, connection, buffer_size=None):
+        """Return a cursor for the given connection
+
+        :param connection: PG connection
+        :param buffer_size: size used when fetching resultsets (None for the default one)
+        :return: cursor
+        """
+        # give the cursor a unique name which will invoke server side cursors
+        # TODO: should this be unique each time or for input?
+        cursor = connection.cursor(name='cur%s' % str(uuid.uuid4()).replace('-', ''),
+                                   cursor_factory=psycopg2.extras.DictCursor)
+        #cursor.tzinfo_factory = None
+        if not buffer_size:
+            cursor.arraysize = self._max_select_mem()
+        else:
+            cursor.arraysize = buffer_size
+        try:
+            cursor.itersize = cursor.arraysize
+        except AttributeError:
+            self.support_itersize = False
+        return cursor
 
     def close_db_conn(self):
         """Explicitly close the connection.
@@ -150,9 +176,16 @@ class PgInput(DbInput):
             DebugPrint(2, "WARNING: No SQL provided: no query.")
             return
         if not self._connection:
-            DebugPrint(2, "WARNING: No connection provided: no query.")
+            DebugPrint(4, "WARNING: No connection provided: trying to (re)open connection.")
+            if not self.open_db_conn():
+                DebugPrint(2, "WARNING: Unable to open connection: no query.")
             return
-        cursor = self._connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        if not self._cursor:
+            self._cursor = self._get_cursor(self._connection)
+            if not self._cursor:
+                DebugPrint(2, "WARNING: Unable to get cursor: no query.")
+                return
+        cursor = self._cursor
         DebugPrint(4, "Executing SQL: %s" % sql)
         cursor.execute(sql)
         if cursor.rowcount is None:
@@ -160,7 +193,15 @@ class PgInput(DbInput):
         elif cursor.rowcount<=0:
             DebugPrint(2, "WARNING, no rows returned by the query: %s" % sql)
         # resultset = self._cur.fetchall()
-        for r in cursor:
-            yield r
-
-
+        if self.support_itersize:
+            for r in cursor:
+                yield r
+        else:
+            # implement itersize manually (for psycopg < 2.4)
+            # normal iteration would be inefficient fetching one record at the time
+            while True:
+                resultset = cursor.fetchmany()
+                if not resultset:
+                    break
+                for r in resultset:
+                    yield r
