3235a24b7c67f897f0f153949922ca7b3a666701
Shivakumar Vinayagam <svinayag>
2015-07-30 21:10:21 +0000

    file checkpointing done
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/branches/dev/aws/probe@5665 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- aws/aws-gratia-probe.py
+++ aws/aws-gratia-probe.py
@@ -6,9 +6,15 @@ from gratia.common.Gratia import DebugPrint
 import boto3;
 from pprint import pprint;
 import datetime
+from time import mktime
+import time
+import sys
 import spot_price
+import cpuutil
+import inst_hardware
+import os
 
-class AWSGratiaProbe:
+class Awsgratiaprobe:
 	def __init__(self):
 		GratiaCore.Config.set_DebugLevel(5)
 	        
@@ -16,12 +22,13 @@ class AWSGratiaProbe:
 	def process(self):
 		ec2=boto3.client('ec2',region_name='us-west-2')	
 		response = ec2.describe_instances()
-		pprint(response)
+		#pprint(response)
 		resv=response['Reservations']
 		for reservation in resv:
 			#pprint(reservation)
 			instances=reservation['Instances']
 			for instance in instances:
+				pprint(instance)
 				print instance['InstanceId']
 				print instance['InstanceType']
 				tags=instance['Tags']
@@ -29,116 +36,188 @@ class AWSGratiaProbe:
 				for tag in tags:
 					print tag['Key'],
 					print tag['Value']
-		
+					
 				r = Gratia.UsageRecord()
+				user="aws account user"
 				for tag in tags:
-					if tag['Key'].lower == 'user'.lower:
-			                	r.LocalUserId(tag['Value'])
-		                keyInfo=vmr.getUserKeyInfo()
-		                if vmr.getMachineName():
-		                    r.MachineName(vmr.getMachineName())
+					if tag['Key'].lower() == 'user'.lower():
+			                	user=tag['Value']	
+				r.LocalUserId(user)
+		                #Public Ip address is retrieved if instance is running"
 				try:
 					ipaddr=instance['PublicIpAddress']
-					r.MachineName(instance['PublicIpAddress'])	
+					r.MachineName(instance['PublicIpAddress'],instance['ImageId'])	
 				except KeyError:
-					r.MachineName("no Public ip as instance has 		stopped")
-					
-		                '''if  keyInfo:
-		                    r.UserKeyInfo(vmr.getUserKeyInfo())
-					we can put the key name used to access
-				'''
+					r.MachineName("no Public ip as instance has been stopped",instance['ImageId'])
+
+
 		                r.LocalJobId(instance['InstanceId'])
 		                r.GlobalJobId(instance['InstanceId']+"#"+repr(time.time()))
+				print 'hello1'
 				for tag in tags:
-					if tag['Key'].lower == 'name'.lower:               
+					if tag['Key'].lower() == 'name'.lower():               
 						r.JobName(tag['Value'])
 				#status,description=recs[i].getStatus()
-				state=instance['State']
-				if state['Name'].lower=='running':
-					status=1
-				else:
-					status=0
-				description=instance['StateReason']['Code']	
-		                r.Status(status,description)
-				r.ProcessorDescription(instance['InstanceType'])
-				r.MachineNameDescription(instance['ImageId'])
-				r.Host(instance['PrivateIpAddress'])
-				r.ReportedSiteName('aws'+instance['Placement']		['AvailabilityZone'])
-				r.ResourceType('aws')
-				for tag in tags:
-					if tag['Key'].lower == 		'project'.lower:               
-						r.ProjectName(tag['Value'])
-					else:
-						r.ProjectName('aws')		
 				
-		                r.Njobs(1,"")
-		                #r.Memory(vmr.getMemory(),"KB")
+				state=instance['State']
+				print state['Name']
+				status=1
+				if state['Name']=="running":
+					print status
+					status=0
+				else:
+					status=1
+				#print 'hello'
+				print status
+				pprint(r)
+				print instance['StateTransitionReason']
+				description=instance['StateTransitionReason']	
+		                print description
+				r.Status(status,description)
+				
+				#r.ProcessorDescription(instance['InstanceType'])
+				#r.MachineNameDescription(instance['ImageId'])
+				
+				try:
+					ipaddr=instance['PrivateIpAddress']
+					r.Host(instance['PrivateIpAddress'],False,instance['Placement']['AvailabilityZone'])
+					r.SubmitHost(instance['PrivateIpAddress'],instance['Placement']['AvailabilityZone'])	
+				except KeyError:
+					r.SubmitHost("no Private ip as instance has been terminated")
+				except Exception as e:
+					print e
+				print 'setting site name'
+				
+				#GratiaCore.Config.setSiteName('aws'+instance['Placement']['AvailabilityZone'])
+				print 'done setting'
+				#r.ReportedSiteName('aws'+instance['Placement']		['AvailabilityZone'])
+				r.ResourceType('aws')
+				project="aws-no project name given"
+				for tag in tags:
+					if tag['Key'].lower() == 'project'.lower():               
+						project=tag['Value']		
+				r.ProjectName(project)
+		                r.Njobs(1,"The no of jobs running at a time")
 		                r.NodeCount(1) # default to total
-				instdet=inst_hardware.insthardware('/home/shiva/pythonsample/hardwareinst')
+				hardwdet=GratiaCore.Config.getConfigAttribute("HardwareDetails")
+				instdet=inst_hardware.insthardware(hardwdet)
 				types=instdet.gettypedetails()
 				pprint(types)
-				
+				processor='1'
+				memory=''
+				price=0.0
 				for t in types:
 					if t['instance-type'] == instance['InstanceType']:
-				processor=t['vcpu']
+						processor=t['vcpu']
 						memory=t['ram']
-				
-		                r.Processors(processor,0,"total")
-		               
-		                # Spot price is retrieved using instance id as the charge 		per hour of that instance in the last hour
+						price=t['price']
+				print memory," the value of memory"	
+				cpu=float(processor)
+		                r.Processors(cpu,0,"total",instance['InstanceType'])
+		               	r.Memory(float(memory))
+		                # Spot price is retrieved using instance id as the charge per hour of that instance in the last hour
 				print instance['InstanceId']
-				sp=spot_price.spot_price()
-				value=sp.get_price(instance['InstanceId'])
-				print value
-				r.Charge(value)
-				r.ChargeUnit("$/instance Hr")
-				r.ChargeDescription()
-				r.ChargeFormula()
-				# The Time period for which the spot price and other 		values are calculated is noted down
+				if "'SpotInstanceRequestId" in instance.keys():
+					sp=spot_price.spot_price()
+					value=sp.get_price(instance['InstanceId'])
+					print value
+					price=value
+				r.Charge(str(price),"$","$/instance hr","The spot price charged in last hour corresponding to launch time")
+				# The Time period for which the spot price and other values are calculated is noted down
 				launchtime=instance['LaunchTime']
 				print launchtime.hour
+				print 'hello3'
 				minu=launchtime.minute
 				print minu
-				EndTime=datetime.datetime.utcnow()
+				
+				currtime=time.time()
+			
+								
+				EndTime=datetime.datetime.now()
+				print type(EndTime)
 				EndTime=EndTime.replace(minute=minu)
 				StartTime=EndTime.replace(hour=(EndTime.hour-1))
 				print StartTime
 				print EndTime
-				
-		                r.StartTime(StartTime)
-		                #etime=current_time
-		                #if float(recs[i].getEndTime())>0:
-		                #    etime=float(recs[i].getEndTime())
-		                r.EndTime(EndTime)
-		                r.WallDuration(EndTime-StartTime)
-				
-				cpu=cpuutil.cpuUtilization()
-				aver=cpu.getUtilPercent(instance['InstanceId'])
-				print aver
-				cpuUtil=aver
-				r.CpuDuration(etime-stime,'user')
+				print 'starttime'
+				t=StartTime.date()
+				#print GratiaCore.TimeToString(time.mktime(t.timetuple()))
+				print 'convert'
+				stime=time.mktime(StartTime.timetuple())
+		                r.StartTime(stime)
+		                
+		                et=EndTime.date()
+				etime=time.mktime(EndTime.timetuple())
+				r.EndTime(etime)
+				print 'hello123'
+				print etime-stime," the diff"
+		                r.WallDuration(etime-stime)
+				Cpu=cpuutil.cpuUtilization()
+				aver=Cpu.getUtilPercent(instance['InstanceId'])
+				print aver," average in percentage"
+				print type(aver)
+				print type(cpu)
+				if aver is None:
+					cpuUtil=0.0
+					print "The CPU Utilization value is NULL as the instance was not running in the last hour"
+					r.CpuDuration(0,'user')
+				else:
+					cpuUtil=aver
+					r.CpuDuration((etime-stime)*float(aver)*cpu/100,'user')
 				r.CpuDuration(0,'system')
-				if recs[i].getSubmitHost() != None:
-		               		r.SubmitHost(recs[i].getSubmitHost())
-				r.ProjectName(self.project)
-		                r.ResourceType(self.resourceType)
-				r.AdditionalInfo("Version",self.version)
+		                r.ResourceType("AWSVM")
+				r.AdditionalInfo("Version","1.0")
 		
-				pprint(r)
-		
-				break
-			break
+				print r
+				print 'done'	
+				Gratia.Send(r)
+				
+			
 
-	if __name__ == '__main__':
-		try:
-			#config=VMGratiaProbeConfig.VMGratiaProbeConfig(opts.gratia_config)
-		    	Gratia.Initialize('/etc/gratia/onevm/ProbeConfig')
-			GratiaWrapper.CheckPreconditions()
-		    	vmProbe=AWSGratiaProbe()
-		    	vmProbe.process()
-		except Exception, e:
-		      	print >> sys.stderr, str(e)
-		        sys.exit(1)
-   		sys.exit(0)	
 
+if __name__ == '__main__':
+	try:
+	    	Gratia.Initialize('/etc/gratia/onevm/ProbeConfig')
+		GratiaWrapper.CheckPreconditions()
+		vmProbe=Awsgratiaprobe()
+		Filelock="filelock"
+		conf=GratiaCore.Config
+		duplicatelock=conf.getConfigAttribute("ExemptDuplicates")
+		filelock=conf.getConfigAttribute("DuplicateFilelock")
+		if  duplicatelock == "True":
+	    		if os.path.isfile(Filelock):
+        			fl=open(Filelock, 'r+')
+	        		date=datetime.datetime.now()
+	        		line=fl.readline()
+	        		print line
+	        		prevdate = datetime.datetime.strptime(line, "%Y-%m-%d %H:%M:%S.%f")
+	        		print prevdate
+	        		currtime=time.mktime(date.timetuple())
+	        		prevtime=time.mktime(prevdate.timetuple())
+	        		diff=currtime-prevtime
+        			print diff
+	        		if diff>=3599.0:
+	                		fl.seek(0, 0)
+        	        		fl.truncate()
+	                		fl.write(str(date));
+	                		t = os.path.getmtime(Filelock)
+	                		print t
+	                		print datetime.datetime.fromtimestamp(t)
+	                		fl.close()
+					vmProbe.process()	
+	        		else:
+			                print "hour is not over yet"
+			                fl.close()
+			else:
+			        fl=open(Filelock,'w+')
+			        date=str(datetime.datetime.now())
+			        fl.write(date);
+			        fl.close()
+				vmProbe.process()
+		else:
+			vmProbe.process()
+	except Exception, e:
+	      	print >> sys.stderr, str(e)
+	        sys.exit(1)
+	sys.exit(0)	
 
