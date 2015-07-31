e7034389be4e668df61820049e343abc0b1ee210
Shivakumar Vinayagam <svinayag>
2015-07-31 15:11:39 +0000

    removed prints and solved tag issues
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/branches/dev/aws/probe@5666 c85a5f45-a5f3-44fc-b02c-7891c381c519

--- aws/cpuutil.py
+++ aws/cpuutil.py
@@ -14,22 +14,22 @@ class cpuUtilization:
 		cw = boto3.client('cloudwatch',region_name='us-west-2')
 		resp=ec2.describe_instances(InstanceIds=[instid])
 		#pprint(resp)
-		print 'hello'
+		#print 'hello'
 		resv=resp['Reservations']
 		for reservation in resv:
 			#pprint(reservation)
 			instances=reservation['Instances']
 			for instance in instances:
-				pprint(instance)
-				print instance['LaunchTime']
+				#pprint(instance)
+				#print instance['LaunchTime']
 				launchtime=instance['LaunchTime']
 				zone=instance['Placement']['AvailabilityZone']
-				print zone
+				#print zone
 				inst_type=instance['InstanceType']
 				print inst_type
-		print launchtime.hour
+		#print launchtime.hour
 		minu=launchtime.minute
-		print minu
+		#print minu
 		EndTime=datetime.datetime.utcnow()
 		EndTime=EndTime.replace(minute=minu)
 		StartTime=EndTime.replace(hour=(EndTime.hour-1))
@@ -50,11 +50,11 @@ class cpuUtilization:
 		    Period=3600,
 		    Statistics=['Average','Minimum','Maximum'],
 		    Unit='Percent')
-		pprint(response)
+		#pprint(response)
 		datapoints=response['Datapoints']
 		if len(datapoints)==1:
 			datapoint=datapoints[0]
 			average=datapoint['Average']
-			print average
+			#print average
 			return average
 
--- aws/spot_price.py
+++ aws/spot_price.py
@@ -15,32 +15,32 @@ class spot_price:
 	def get_price(self,instid):
 		resp=self.ec2.describe_instances(InstanceIds=[instid])
 		#pprint(resp)
-		print 'hello'
+		#print 'hello'
 		resv=resp['Reservations']
 		for reservation in resv:
 			#pprint(reservation)
 			instances=reservation['Instances']
 			for instance in instances:
-				pprint(instance)
-				print instance['LaunchTime']
+				#pprint(instance)
+				#print instance['LaunchTime']
 				launchtime=instance['LaunchTime']
 				zone=instance['Placement']['AvailabilityZone']
-				print zone
+				#print zone
 				inst_type=instance['InstanceType']
-				print inst_type
-		print launchtime.hour
+				#print inst_type
+		#print launchtime.hour
 		minu=launchtime.minute
-		print minu
+		#print minu
 		StartTime=datetime.datetime.utcnow()
-		print StartTime
+		#print StartTime
 		if(minu>StartTime.minute):
 			StartTime=StartTime.replace(hour=(StartTime.hour-1))
 		StartTime=StartTime.replace(minute=minu)
-		print StartTime
+		#print StartTime
 		EndTime=StartTime
 		#response = self.ec2.describe_spot_price_history(StartTime=datetime(2015,7,8,9,00,00),EndTime=datetime(2015,7,8,9,00,00),InstanceTypes=['m3.medium'],ProductDescription=['Linux/UNIX'],AvailabilityZone='us-west-2a',NextToken='abc')
 		#pprint(response)
-		print 'hello'
+		#print 'hello'
 		resp1 = self.ec2.describe_spot_price_history(
                 DryRun=False,
                 StartTime=StartTime,
@@ -51,7 +51,7 @@ class spot_price:
                 AvailabilityZone=zone,
                 MaxResults=1000,
                 NextToken='')
-		pprint(resp1)
+		#pprint(resp1)
 		sphs=resp1['SpotPriceHistory']
 		if len(sphs)==0:
 			print "no spot price history as Instance is not of a spot Instance type"
@@ -59,15 +59,15 @@ class spot_price:
 		else:
 			sph=sphs[0]
 			spotprice=sph['SpotPrice']
-		print spotprice
-		print 'hello'
+		#print spotprice
+		#print 'hello'
 		return spotprice
 		return StartTime
 if __name__ == '__main__':
 	try:
 		sp=spot_price()
 		value=sp.get_price('i-d0952f26')
-		print value
+		#print value
 	except IndexError:
 		print "The instance is not a spot type instance and Hence there is no spot price history"
 	except Exception, e:
