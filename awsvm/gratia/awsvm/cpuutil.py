93f18661c46dddf4b10fb0792a0f9f1fa93ad904
Shivakumar Vinayagam <svinayag>
2015-08-04 21:03:39 +0000

    rearranged structure
    git-svn-id: http://svn.code.sf.net/p/gratia/svn/branches/dev/aws/probe@5667 c85a5f45-a5f3-44fc-b02c-7891c381c519

---
+++ awsvm/gratia/awsvm/cpuutil.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+from pprint import pprint
+import datetime;
+import boto3;
+import sys
+import os
+import time
+
+class cpuUtilization:
+	def __init__(self):
+		self.ec2=boto3.client('ec2',region_name='us-west-2')
+	def getUtilPercent(self,instid):
+		ec2=boto3.client('ec2',region_name='us-west-2')
+		cw = boto3.client('cloudwatch',region_name='us-west-2')
+		resp=ec2.describe_instances(InstanceIds=[instid])
+		#pprint(resp)
+		#print 'hello'
+		resv=resp['Reservations']
+		for reservation in resv:
+			#pprint(reservation)
+			instances=reservation['Instances']
+			for instance in instances:
+				#pprint(instance)
+				#print instance['LaunchTime']
+				launchtime=instance['LaunchTime']
+				zone=instance['Placement']['AvailabilityZone']
+				#print zone
+				inst_type=instance['InstanceType']
+				print inst_type
+		#print launchtime.hour
+		minu=launchtime.minute
+		#print minu
+		EndTime=datetime.datetime.utcnow()
+		EndTime=EndTime.replace(minute=minu)
+		StartTime=EndTime.replace(hour=(EndTime.hour-1))
+		print StartTime
+		print EndTime
+		
+		response = cw.get_metric_statistics(
+		    Namespace='AWS/EC2',
+		    MetricName='CPUUtilization',
+		    Dimensions=[
+        		{
+		            'Name': 'InstanceId',
+		            'Value': instid
+		        },
+		    ],
+		    StartTime=StartTime,
+		    EndTime=EndTime,
+		    Period=3600,
+		    Statistics=['Average','Minimum','Maximum'],
+		    Unit='Percent')
+		#pprint(response)
+		datapoints=response['Datapoints']
+		if len(datapoints)==1:
+			datapoint=datapoints[0]
+			average=datapoint['Average']
+			#print average
+			return average
+
