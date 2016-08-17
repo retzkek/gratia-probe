f05271e3e359624edc4d89833819cc6a5cb2b043
Brian Bockelman <bbockelm@cse.unl.edu>
2016-08-17 16:03:19 -0500

    Improved logic for handling split transfers.

--- test/test_driver.sh
+++ test/test_driver.sh
@@ -2,11 +2,16 @@
 
 rm -rf testing/
 mkdir -p testing/{tmp,data,log}
+mkdir -p testing/data/quarantine
 
 
 sed -e "s|@PWD@|$PWD|" -e "s|@TESTFILE@|simple-xfer.log|" ProbeConfigTemplate > ProbeConfig
 ../gratia-probe-gridftp -f ProbeConfig
 
 sed -e "s|@PWD@|$PWD|" -e "s|@TESTFILE@|simple-split-xfer.log|" ProbeConfigTemplate > ProbeConfig
+../gratia-probe-gridftp -f ProbeConfig
+
+echo "******************************************************************************"
+
 ../gratia-probe-gridftp -f ProbeConfig
 
